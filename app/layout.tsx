bac84eb7733b3c06f38073b25bd64a8385965bfa
chachanisya <2206168@itg.ac.id>
2025-06-14 14:07:59 +0700

    fixed error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700"],
